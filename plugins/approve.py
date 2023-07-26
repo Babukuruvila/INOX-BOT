a5cf7e8f7f6a1932b6a8736b6596187e79355116
Babukuruvila <bobbyjohnamion@gmail.com>
2023-07-26 16:08:06 +0530

    Back To Normal

--- plugins/approve.py
+++ plugins/approve.py
@@ -12,8 +12,9 @@ async def accept_request(client, r):
     ]])
     
     try:
-        await client.send_text(
+        await client.send_photo(
             r.from_user.id,
+            'https://telegra.ph/file/ad1ed13c9f03f9d9bd27a.jpg',
             f"**𝖧𝖾𝗅𝗅𝗈👋🏻 {r.from_user.mention} 😍, 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title}\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽...!!!**",
             reply_markup=rm)
 
